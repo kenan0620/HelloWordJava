9552cfadddf38fb35dcd42dca750fe6688bc26e9
coenen <coenen@aliyun.com>
2020-10-19 19:45:44 +0800

    变量和数据类型

--- src/HelloWordJava.java
+++ src/HelloWordJava.java
@@ -13,6 +13,26 @@ public class HelloWordJava {
 		 * 
 		 * */
 		System.out.println("HelloWordJava!");
+		
+		int x = 100;
+		System.out.println(x);
+		x = 200;
+		System.out.println(x);
+		x = 300;
+		System.out.println(x);
+		
+		int n = 100; // 定义变量n，同时赋值为100
+        System.out.println("n = " + n); // 打印n的值
+
+        n = 200; // 变量n赋值为200
+        System.out.println("n = " + n); // 打印n的值
+
+        int y = n; // 变量x赋值为n（n的值为200，因此赋值后y的值也是200）
+        System.out.println("y = " + y); // 打印y的值
+
+        y = y + 100; // 变量x赋值为y+100（y的值为200，因此赋值后y的值是200+100=300）
+        System.out.println("y = " + y); // 打印y的值
+        System.out.println("n = " + n); // 再次打印n的值，n应该是200还是300？
 	}
 
 }
