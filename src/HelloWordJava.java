f4e4fa81ac8a32fefa40eddb12d62e939911835e
coenen <coenen@aliyun.com>
2020-10-19 19:29:45 +0800

    注释使用

--- src/HelloWordJava.java
+++ src/HelloWordJava.java
@@ -1,6 +1,17 @@
 
+/**
+ * 开始练习JAVA
+ * 
+ * @author conan
+ * */
+
 public class HelloWordJava {
+	//Java入口程序规定的方法必须是静态方法，方法名必须为main，括号内的参数必须是String数组
 	static public void main(String[] args) {
+		/*
+		 * 
+		 * 
+		 * */
 		System.out.println("HelloWordJava!");
 	}
 
