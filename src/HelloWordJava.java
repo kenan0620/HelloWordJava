0fd13a574e23d8ef75115a6dfbd5381d176be275
coenen <coenen@aliyun.com>
2020-10-20 11:14:27 +0800

    变量和数据类型

--- src/HelloWordJava.java
+++ src/HelloWordJava.java
@@ -33,6 +33,33 @@ public class HelloWordJava {
         y = y + 100; // 变量x赋值为y+100（y的值为200，因此赋值后y的值是200+100=300）
         System.out.println("y = " + y); // 打印y的值
         System.out.println("n = " + n); // 再次打印n的值，n应该是200还是300？
+        
+        int m = (100 + 200) * (50 + 32);
+        int z = 7 * (6 + (m - 6234));
+        System.out.println(m);
+        System.out.println(z);
+        
+        int a = z / 53;
+        System.out.println(a);
+        int b = z % 53;
+        System.out.println(b);
+        
+        char k = 'A';
+        char l = '中';
+        System.out.println(k);
+        System.out.println(l);
+        
+        final double PI = 3.141592;
+        double r = 5.0;
+        double area = r * r * PI;
+        
+        System.out.println(area);
+        
+        var num = 9 ;
+        System.out.println(num);
+        
+        var hello = new HelloWordJava();
+        System.out.println(hello.toString());
 	}
 
 }
