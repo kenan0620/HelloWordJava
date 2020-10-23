b93c8e49b0d2844208b0f5e765d85f777e81b337
coenen <coenen@aliyun.com>
2020-10-23 15:09:00 +0800

    多态

--- src/Person.java
+++ src/Person.java
@@ -4,7 +4,7 @@ public class Person {
 	protected int age;
 
 	private String[] names;
-	
+
 	public String getName() {
 		/*
 		 * 在方法内部，可以使用一个隐含的变量this，它始终指向当前实例。因此，通过this.field就可以访问当前实例的字段。
@@ -31,7 +31,7 @@ public class Person {
 		}
 		this.age = age;
 	}
-	
+
 	public void setNameAndAge(String name, int age) {
 		this.name = name;
 		if (age < 0 || age > 130) {
@@ -39,41 +39,53 @@ public class Person {
 		}
 		this.age = age;
 	}
-	
+
 	public void setNames(String... names) {
 		this.names = names;
 	}
-	
+
 	public Person(String name, int age) {
-		this.name  = name;
+		this.name = name;
 		this.age = age;
 	}
-	 // 调用另一个构造方法Person(String)
+
+	// 调用另一个构造方法Person(String)
 	public Person() {
 		this("no");
 	}
+
 	// 调用另一个构造方法Person(String, int)
 	public Person(String name) {
 		this(name, 18);
 	}
 
+	/*
+	 * 用final修饰的方法不能被Override： 用final修饰的类不能被继承：
+	 */
+	public final void custom() {
+
+	}
 }
 
-class Student extends Person{
+final class Teacher extends Person {
+
+}
+
+class Student extends Person {
 	private int score;
-	
+
 	public int getScore() {
 		return score;
 	}
-	
+
 	public void setScore(int score) {
 		this.score = score;
 	}
-	
+
 	public String hello() {
 		return "Hello, " + super.name;
 	}
-	
+
 	public Student(String name, int age, int score) {
 		super();
 		this.score = score;
